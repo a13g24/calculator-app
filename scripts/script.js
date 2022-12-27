5f68a1974c2f44d03d3b663cbb41346e389cbfeb
Aaron Gomez <a13g24@gmail.com>
2022-12-27 15:28:40 -0700

    Finished theme switcher in js

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,17 @@ switches.forEach( (el, index) => {
     el.addEventListener('click', () => {
         el.style.opacity = '1';
 
+        // let currTheme = document.documentElement.getAttribute('data-theme');
+
+        if (index == 0) {
+            document.documentElement.setAttribute('data-theme', 'theme-one');
+        } else if (index == 1) {
+            document.documentElement.setAttribute('data-theme', 'theme-two');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'theme-three');
+        }
+
+
         switches
             .filter(function (item) {
                 return item != el;
