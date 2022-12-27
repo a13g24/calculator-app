a2df98786aa95eedc229f25334479becb3b3fdf3
Aaron Gomez <a13g24@gmail.com>
2022-12-27 14:40:15 -0700

    Finished toggle switch js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 let toggleSwitch = document.getElementsByClassName('tri-state-toggle__button');
+let switches = [...toggleSwitch];
+
+// dynamically change opacity to simulate 3-toggle switch
+switches.forEach( (el, index) => {
+    el.addEventListener('click', () => {
+        el.style.opacity = '1';
+
+        switches
+            .filter(function (item) {
+                return item != el;
+            })
+            .forEach((item) => {
+                item.style.opacity = '0';
+            });
+    });
+});
