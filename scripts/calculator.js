c2f76c8d0c8ea33e80053a0a772d6cee488b20d2
Aaron Gomez <a13g24@gmail.com>
2022-12-27 23:32:04 -0700

    Finished handling operators and numbers

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,6 @@
 let calcKeys = document.querySelector('.calculator__keys');
 let screen = document.querySelector('.calculator__screen');
-// TODO: use array to push each number entered (sep by op) on stack
+// let operandList = [];
 
 // Handle all key click events
 calcKeys.addEventListener('click', (e) => {
@@ -18,7 +18,7 @@ calcKeys.addEventListener('click', (e) => {
             break;
         case 'other':
         // Handle others ., all-clear, =, delete
-            console.log('Handling other key');
+            handleOther(selectedKey);
     }
 });
 
@@ -32,15 +32,28 @@ function clearCommas(s) {
 }
 
 /**
+ * Helper function to determine if a char is an operator.
+ * @param {string} op - The operator to test
+ * @returns 
+ */
+function isAnOperator(op) {
+    return '+-/*'.includes(op);
+}
+
+/**
  * Handles number entries into screen and comma formatting.
  * @param {Object} numKey - The target element
  */
 function handleNumber(numKey) {
     let enteredValue = numKey.value;
+    let lastChar = screen.value[screen.value.length - 1];
 
     // handle special case of only entry being 0
     if (screen.value === '0') {
         screen.value = "";
+    } else if (isAnOperator(lastChar) && enteredValue === '0') {
+    // handle case when 0 is input after operator
+        return;
     }
 
     // eliminate prev commas 
@@ -51,25 +64,43 @@ function handleNumber(numKey) {
 
     // insert new commas
     // See https://linuxhint.com/add-commas-number-javascript/#:~:text=JavaScript%20provides%20toLocaleString()%2C%20regex,a%20comma%20after%20the%20digit
-    screen.value = screen.value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const regex = /\B(?=(\d{3})+(?!\d))/g;
+    screen.value = screen.value.replace(regex, ',');
 }
 
-
-// Operator handler
+/**
+ * Handles keys that are operators
+ * @param {Object} opKey - A key object whose value is an operator
+ */
 function handleOperator(opKey) {
-    console.log('Handling operator key');
+    let lastChar = screen.value[screen.value.length - 1];
 
-    switch (opKey.value) {
-        case '+':
-            console.log('add');
+    // handle special case when operator entered twice
+    if (isAnOperator(lastChar)) {
+        let lastDex = screen.value.length - 1;
+
+        // get last value in screen and change it to entered value
+        screen.value = screen.value.substring(0, lastDex) + opKey.value;
+    } else {
+        screen.value += opKey.value;
+    }
+}
+
+// Handle other keys
+function handleOther(other) {
+    console.log('Handling other key');
+
+    switch (other.classList[1]) {
+        case 'decimal':
+            console.log('decimal');
             break;
-        case '-':
-            console.log('sub');
+        case 'all-clear':
+            console.log('AC');
             break;
-        case '*':
-            console.log('mult');
+        case 'equal-sign':
+            console.log('equals');
             break;
-        case '/':
-            console.log('div');
+        case 'delete':
+            console.log('delete');
     }
 }
